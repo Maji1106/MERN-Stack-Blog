adc119713ce5241639ab4ce47f2490a49bc31d5d
maji1106 <nnnpphotmail.com@gmail.com>
2024-12-12 11:57:04 +0700

    12/12

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import './index.css'
-import App from './App.jsx'
-import Router from './routes/router.jsx'
+import "./index.css";
+import App from "./App.jsx";
+import Router from "./routes/Router.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <RouterProvider router={Router} />
-  </StrictMode>,
-)
+    <RouterProvider router={Router} />
+  </StrictMode>
+);
